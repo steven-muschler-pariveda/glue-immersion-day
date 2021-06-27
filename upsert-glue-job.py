17570f7d1764b8c545c7b1793f38fab681ec87bf
steven-muschler-pariveda <84201106+steven-muschler-pariveda@users.noreply.github.com>
2021-06-26 23:01:50 -0500

    Update upsert-glue-job.py

--- upsert-glue-job.py
+++ upsert-glue-job.py
@@ -16,13 +16,13 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 ## @type: DataSource
-## @args: [format_options = {"jsonPath":"","multiline":False}, connection_type = "s3", format = "json", connection_options = {"paths": ["s3://jd-immersion-day-test-dincher/dms-replicate/labdb/customers/"], "recurse":True}, transformation_ctx = "DataSource0"]
+## @args: [format_options = {"jsonPath":"","multiline":False}, connection_type = "s3", format = "json", connection_options = {"paths": ["s3://INSERT_BUCKET_NAME/dms-replicate/labdb/customers/"], "recurse":True}, transformation_ctx = "DataSource0"]
 ## @return: DataSource0
 ## @inputs: []
-Updates = glueContext.create_dynamic_frame.from_options(format_options = {"jsonPath":"","multiline":False}, connection_type = "s3", format = "json", connection_options = {"paths": ["s3://jd-immersion-day-test-dincher/dms_updates/"], "recurse":True}, transformation_ctx = "Updates")
+Updates = glueContext.create_dynamic_frame.from_options(format_options = {"jsonPath":"","multiline":False}, connection_type = "s3", format = "json", connection_options = {"paths": ["s3://INSERT_BUCKET_NAME/dms_updates/"], "recurse":True}, transformation_ctx = "Updates")
 
-Records = spark.read.load("s3a://jd-immersion-day-test-dincher/dms_parquet/")
-#= glueContext.create_dynamic_frame.from_options(format_options = {"jsonPath":"","multiline":False}, connection_type = "s3", format = "parquet", connection_options = {"paths": ["s3://jd-immersion-day-test-dincher/dms_parquet/"], "recurse":True}, transformation_ctx = "Records").toDF()
+Records = spark.read.load("s3a://INSERT_BUCKET_NAME/dms_parquet/")
+#= glueContext.create_dynamic_frame.from_options(format_options = {"jsonPath":"","multiline":False}, connection_type = "s3", format = "parquet", connection_options = {"paths": ["s3://INSERT_BUCKET_NAME/dms_parquet/"], "recurse":True}, transformation_ctx = "Records").toDF()
 ## @type: ApplyMapping
 ## @args: [mappings = [("operation", "string", "operation", "string"), ("data._id.$oid", "string", "data._id.oid", "string"), ("data.custid", "int", "data.custid", "int"), ("data.trafficfrom", "string", "data.trafficfrom", "string"), ("data.url", "string", "data.url", "string"), ("data.device", "string", "data.device", "string"), ("data.touchproduct", "int", "data.touchproduct", "int"), ("data.trans_timestamp", "string", "data.trans_timestamp", "string")], transformation_ctx = "Transform0"]
 ## @return: Transform0
@@ -39,10 +39,10 @@ MergedDataFrame = MergedDataFrame.withColumn("_row_number", row_number().over(Wi
 
 MergedDataFrame = MergedDataFrame.where(MergedDataFrame._row_number == 1).drop("_row_number")
 
-MergedDataFrame.write.option("compression","gzip").partitionBy("year_month").parquet("s3://jd-immersion-day-test-dincher/dms_temp/customer/",mode="overwrite")
+MergedDataFrame.write.option("compression","gzip").partitionBy("year_month").parquet("s3://INSERT_BUCKET_NAME/dms_temp/customer/",mode="overwrite")
 
-Output = spark.read.load("s3a://jd-immersion-day-test-dincher/dms_temp/")
+Output = spark.read.load("s3a://INSERT_BUCKET_NAME/dms_temp/")
 
-Output.write.option("compression","gzip").partitionBy("year_month").parquet("s3://jd-immersion-day-test-dincher/dms_parquet/customer/",mode="overwrite")
+Output.write.option("compression","gzip").partitionBy("year_month").parquet("s3://INSERT_BUCKET_NAME/dms_parquet/customer/",mode="overwrite")
 
 job.commit()
